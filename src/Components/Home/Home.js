edb5fba00aca83762753b3fe06d50d07ac61ad79
Mike De Groot <mdegroot09@gmail.com>
2019-05-24 21:43:19 -0600

    added tree to Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react'
+import Tree from 'react-sortable-tree'
 
 export default class extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      treeData: [{ title: '', children: [{ title: '' }] }],
+      components: [{}]
+    }
+  }
+  
   render(){
     return(
       <div>
         Home
+        <Tree
+            treeData={this.state.treeData}
+            onChange={treeData => this.setState({treeData})}
+            onMoveNode={(e) => this.updateParentId(e)}
+            />
       </div>
     )
   }
